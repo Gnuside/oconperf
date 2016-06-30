3c81667c6f9696699209e1b393444cd4997de729
Jérôme Arzel <jerome.arzel@gnuside.com>
2016-06-30 10:48:27 +0200

    Removing double semicolons, aligning arguments in --help page.

--- src/oconperf.ml
+++ src/oconperf.ml
@@ -14,46 +14,48 @@ let show_version () =
   print_endline (sprintf "Version: %s" version);
   exit 0
 
-let args = ref [
-  ("--version", Arg.Unit(show_version), "\tShow software version");
-  ("-q", Arg.Set(quiet), "\tRun in quiet and parseable mode");
-  ("--debug", Arg.Set(debug), "\tRun in debug mode");
-  ("-p", Arg.Set_int(port), "\tSet server port (to connect to ; on to listen to)");
-  ("-a", Arg.Set_string(addr), "\tSet server address (to connect to ; on to listen to)");
-  ("-S", Arg.Set_int(max_packet_size), "\tSet maximum packet size");
-]
-;;
+let args = ref (Arg.align [
+  ("--version", Arg.Unit(show_version), " Show software version");
+  ("-q", Arg.Set(quiet), " Run in quiet and parseable mode");
+  ("--debug", Arg.Set(debug), " Run in debug mode");
+  ("-p", Arg.Set_int(port), " Set server port (to connect to ; on to listen to)");
+  ("-a", Arg.Set_string(addr), " Set server address (to connect to ; on to listen to)");
+  ("-S", Arg.Set_int(max_packet_size), " Set maximum packet size");
+])
 
 let server_args = !args
-;;
 
-let client_args = !args @ [
-  ("-I", Arg.Set_string(iface_name), "\tSet interface name");
-  ("-w", Arg.Set_int(max_timeout), "\tSet maximum timeout");
-  ("-s", Arg.Set_int(max_size), "\tSet maximum downloaded size");
-  ("-u", Arg.Set(test_upload), "\tTest upload speed instead of download");
-  ("-h", Arg.Set(human_readable), "\tPrint sizes in powers of 1024 (e.g., 1023M)");
+and client_args = !args @ [
+  ("-I", Arg.Set_string(iface_name), " Set interface name");
+  ("-w", Arg.Set_int(max_timeout), " Set maximum timeout");
+  ("-s", Arg.Set_int(max_size), " Set maximum downloaded size");
+  ("-u", Arg.Set(test_upload), " Test upload speed instead of download");
+  ("-h", Arg.Set(human_readable), " Print sizes in powers of 1024 (e.g., 1023M)");
 ]
-;;
 
 let anon_fun arg = match !Arg.current with
  | 1 -> begin
   match arg with
-   | "server" -> running := Some(Server) ; args := server_args
-   | "client" -> running := Some(Client) ; args := client_args
+   | "server" -> begin
+    running := Some(Server) ;
+    args := Arg.align server_args
+   end
+   | "client" -> begin
+    running := Some(Client) ;
+    args := Arg.align client_args
+   end
    | _ -> failwith (sprintf "Unknown action '%s'" arg)
   end
  | _  -> failwith (sprintf "Argument '%s' ignored." arg)
-;;
 
-Arg.parse_dynamic args anon_fun usage;;
-
-iface := match !iface_name with
- | ""   -> `Any
- | name -> (`Interface_name name)
-;;
+let parse_arguments () =
+  Arg.parse_dynamic args (anon_fun) usage ;
+  iface := match !iface_name with
+   | ""   -> `Any
+   | name -> (`Interface_name name)
 
 let _ =
+  parse_arguments () ;
   exit (Unix.handle_unix_error (fun () -> begin
     match !running with
      | Some(Server) -> Oconperf_server.run ~max_packet_size: !max_packet_size
@@ -73,4 +75,3 @@ let _ =
          failwith "Please specify the action argument."
        )
   end) ())
-;;
