c1f8fc92a7e4024e6163a840f00911a2e39e7a70
Jérôme Arzel <jerome.arzel@gnuside.com>
2016-07-15 16:27:53 +0200

    Force child to quit

--- liboconperf/oconperf_protocol.ml
+++ liboconperf/oconperf_protocol.ml
@@ -201,6 +201,7 @@ let client_run ?(test_upload=false) ?(max_time=2.0) ?(max_size=0) ?(max_packet_s
         end
       in wait_until_child_writes () ;
       close read_fd ;
+      kill pid 15 ; (* SIGTERM *)
       ignore (waitpid [] pid)
 
     in begin match fork() with
