cdb597f855d8c7b043fddffc88a0faeff5bbeff6
Glenn Y. Rolland <glenux@glenux.net>
2016-09-08 17:52:48 +0200

    Prepare for function separation.

--- liboconperf/oconperf_client.ml
+++ liboconperf/oconperf_client.ml
@@ -10,53 +10,42 @@ let connect_to ~iface ~max_time addr port =
 
   print_message_f (fun () -> (sprintf "Client connects to %s:%d" addr port)) ;
 
-  let s = socket addr_info.ai_family SOCK_STREAM 0 in
+  let so = socket addr_info.ai_family SOCK_STREAM 0 in
   begin match iface with
   | `Any -> () (* Any is the default behavior of socket,
                   and bind_to_interface needs root perms *)
-  | _    -> Std.Or_error.ok_exn Linux_ext.bind_to_interface s iface
+  | _    -> Std.Or_error.ok_exn Linux_ext.bind_to_interface so iface
   end ;
   (* Set timeout *)
   if max_time > 0.0 then begin
-    setsockopt_float s SO_RCVTIMEO max_time ;
-    setsockopt_float s SO_SNDTIMEO max_time
+    setsockopt_float so SO_RCVTIMEO max_time ;
+    setsockopt_float so SO_SNDTIMEO max_time
   end ;
-  let connect_retry () =
-    try connect s addr_info.ai_addr with 
+  let connect_retry sock =
+    printf "oconperf(55): trying connect with timeout=%f\n%!" max_time ;
+    try connect sock addr_info.ai_addr with 
     | Unix_error (EINPROGRESS, m, a) -> begin
-        print_endline "oconperf(54): wait to connect..." ;
-        match select [] [s] [] max_time with
-        | _, [_], _ -> 
-          begin
-            match getsockopt_error s with
+        print_endline "oconperf(55): wait to connect..." ;
+        match select [] [sock] [] max_time with
+        | _, [_], _ -> begin
+            match getsockopt_error sock with
             | Some e -> ( 
-                print_endline "oconperf(54): select error..." ;
+                print_endline "oconperf(55): select error..." ;
                 raise (Unix_error(e, m, a))
               )
             | None   -> (* Connected *)
               (
-                print_endline "oconperf(54): connected..."
+                print_endline "oconperf(55): connected..."
               ) 
           end
-        | [_], _, _ -> (* Becomes readable ? *)
-          (
-            print_endline "oconperf(54): readable..." ;
-            raise (Unix_error(EINPROGRESS, m, a))
-          ) 
-        | _, _, [_] -> (* Exceptional condition ? *)
-          (
-            print_endline "oconperf(54): exceptional condition..." ;
-            raise (Unix_error(EINPROGRESS, m, a))
-          ) 
-        | _ -> (* timeout *)
-          (
-            print_endline "oconperf(54): timeout..." ;
-            raise (Unix_error(EINPROGRESS, m, a))
-          ) 
+        | _ -> begin (* timeout *)
+            print_endline "oconperf(55): timeout..." ;
+            raise (Unix_error(EINPROGRESS, m, a)) 
+          end
       end
   in
-  connect_retry () ;
-  s
+  connect_retry so ;
+  so
 
 let speed_test ?(test_upload=false) ?(max_time=2.0) ?(max_size=0) ?(max_packet_size=0) ?(iface=`Any) addr port =
   let s = connect_to addr port ~iface: iface ~max_time: max_time in
